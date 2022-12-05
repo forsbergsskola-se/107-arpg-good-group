01bb838cc85852b39b335c21eb44ac6122c2428c
Jacob-Roming <jacob.roming@student.forsbergsskola.se>
2022-12-05 15:04:31 +0100

    Adds a script with skeleton code

---
+++ Slappy-Knut/Assets/Scripts/NPC.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public bool canFlee;
+    public bool canAttack;
+
+    public float movementSpeed;
+    public float attackSpeed;
+    //public Item[] loot; TODO: uncomment this when items are finished
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Flee()
+    {
+        
+    }
+
+    void Roam()
+    {
+        
+    }
+
+    void Attack()
+    {
+        
+    }
+
+    void RandomizeValues()
+    {
+        
+    }
+
+    void DropLoot()
+    {
+        
+    }
+}
