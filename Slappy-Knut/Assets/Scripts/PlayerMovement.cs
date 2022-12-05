def1822bd165ae4a742caffcd08ec0fae24d0460
nihanbir <nihan.bir@gmail.com>
2022-12-05 15:18:51 +0100

    #001 Adds player movement
    Move on click

---
+++ Slappy-Knut/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LayerMask walkableLayer;
+    public int maxRayCastDistance = 100;
+    private NavMeshAgent _meshAgent;
+
+    private void Start()
+    {
+        _meshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // right click
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo, maxRayCastDistance, walkableLayer))
+            {
+                _meshAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
