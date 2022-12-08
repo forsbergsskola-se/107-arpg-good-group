04033ee1863bc5cbaca2e7ecc55ce5d2580a6ee5
nihanbir <nihan.bir@gmail.com>
2022-12-08 13:38:56 +0100

    #500 Makes the terrain walkable

---
+++ Slappy-Knut/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LayerMask walkableLayer;
+    public int maxRayCastDistance = 100;
+    private NavMeshAgent _meshAgent;
+    private void Start()
+    {
+        _meshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) // left click
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayOrigin, out hitInfo, maxRayCastDistance, walkableLayer))
+            {
+                _meshAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
