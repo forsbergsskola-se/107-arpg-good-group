ce7ec95fb58d3cf47112ef89c07037a8d26670e4
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-12-08 12:15:14 +0100

    #005 - Fixes Item class and moves it to scripts folder

---
+++ Slappy-Knut/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    public abstract bool Chargeable { get; }
+    public abstract float Power { get; }
+    public abstract string Description { get; }
+    public abstract float Cooldown { get; }
+    public abstract float Range { get; }
+
+    public abstract void Use();
+    public abstract void Charge();
+}
