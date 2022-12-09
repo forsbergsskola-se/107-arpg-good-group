3aa8225478380bff7cfce82442f15450f2ffe428
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-12-09 23:28:47 +0100

    #800 - Update camera lookat fix so the healthbar doesn't rotate

--- Slappy-Knut/Assets/Scripts/HealthBar.cs
+++ Slappy-Knut/Assets/Scripts/HealthBar.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     private GameObject _camera;
+    private OgreBoss _ogre;
     private void Start()
     {
+        _ogre = FindObjectOfType<OgreBoss>();
         _camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
     private void LateUpdate()
     {
         //Todo: Needs to look at the camera but not turn around when the camera runs past the boss
-        gameObject.transform.LookAt(_camera.transform.position);
+        //gameObject.transform.LookAt(_camera.transform.position);
+        //this working better
+        transform.LookAt(_ogre.transform.position + _camera.transform.position);
     }
 }
