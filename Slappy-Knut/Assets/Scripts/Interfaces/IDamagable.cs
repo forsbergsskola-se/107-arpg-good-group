9b690a2699cdf3713587bfa6f2f203ca4a644f86
QaleQ <aleflard@gmail.com>
2022-12-13 23:11:13 +0100

    Fix IDamagable method name to reflect TDD

--- Slappy-Knut/Assets/Scripts/Interfaces/IDamagable.cs
+++ Slappy-Knut/Assets/Scripts/Interfaces/IDamagable.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
-public interface IDamagable
+
+namespace Interfaces
 {
-    float DefenseRating { get; set; }
-    void TakeDamage(float damage, GameObject attacker);
-    void OnHealthZero();
+    public interface IDamagable
+    {
+        float DefenseRating { get; set; }
+        
+        void TakeDamage(float damage, GameObject attacker);
+        void OnDeath();
+    }
 }
