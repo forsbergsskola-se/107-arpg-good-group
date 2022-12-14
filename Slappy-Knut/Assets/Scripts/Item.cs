49f6a6186330287d470b724bf4da086777615e6d
QaleQ <aleflard@gmail.com>
2022-12-14 10:35:07 +0100

    Fixes Item class missing setters
    Setters are always required in unity, apparently

--- Slappy-Knut/Assets/Scripts/Item.cs
+++ Slappy-Knut/Assets/Scripts/Item.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
-    public abstract bool Chargeable { get; }
-    public abstract float Power { get; }
-    public abstract string Description { get; }
-    public abstract float Cooldown { get; }
-    public abstract float Range { get; }
+    public abstract bool Chargeable { get; set; }
+    public abstract float Power { get; set; }
+    public abstract string Description { get; set; }
+    public abstract float Cooldown { get; set; }
+    public abstract float Range { get; set; }
 
     public abstract void Use();
     public abstract void Charge();
