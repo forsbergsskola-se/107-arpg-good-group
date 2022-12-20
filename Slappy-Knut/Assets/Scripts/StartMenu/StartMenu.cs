628aa2b71c51fcdd366d1079c42055b99d370a02
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-12-20 14:44:58 +0100

    #501 Improves main menu

--- Slappy-Knut/Assets/Scripts/StartMenu/StartMenu.cs
+++ Slappy-Knut/Assets/Scripts/StartMenu/StartMenu.cs
@@ -6,24 +6,20 @@ public class StartMenu : MonoBehaviour
     private GameObject _player;
     private GameObject _inventoryUi;
     private GameObject _inventoryManager;
-    private GameObject _canvas;
+    public GameObject canvas;
     public void StartGame()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         _inventoryUi = FindObjectOfType<InventoryUI>().gameObject;
         _inventoryManager = FindObjectOfType<Inventory>().gameObject;
-        _canvas = FindObjectOfType<Canvas>().gameObject;
-        
+
         DontDestroyOnLoad(_player);
         DontDestroyOnLoad(_inventoryUi);
         DontDestroyOnLoad(_inventoryManager);
-        DontDestroyOnLoad(_canvas);
+        DontDestroyOnLoad(canvas);
         
         SceneManager.LoadScene("The_Viking_Village");
-        _player.SetActive(true);
-        _inventoryUi.SetActive(true);
-        _inventoryManager.SetActive(true);
-        _canvas.SetActive(true);
+        canvas.GetComponent<Canvas>().enabled = true;
         _player.transform.position = new Vector3(-85,5,12);
     }
 }
