af97230d35d5ab3d21b1438bd8f9325057d3adf8
QaleQ <aleflard@gmail.com>
2022-12-21 18:24:35 +0100

    #011 Refactors the Weapon class
    new standard for classes inheriting from Weapon

--- Slappy-Knut/Assets/Scripts/Weapons/Glove.cs
+++ Slappy-Knut/Assets/Scripts/Weapons/Glove.cs
@@ -2,19 +2,28 @@ using UnityEngine;
 
 public class Glove : Weapon
 {
-    public Sprite icon;
-    [SerializeField] private float _power = 4f;
-    public GameObject glovePrefab;
-    private void Start()
+    public override string Name { get; set; }
+    public override Sprite Icon { get; set; }
+    public override float Power { get; set; }
+    public override float Range { get; set; }
+    public override bool Chargable { get; set; }
+    public override string Description { get; set; }
+    public override float Cooldown { get; set; }
+    public override float ChargeTime { get; set; }
+    public override bool IsEquipped { get; set; }
+
+    [SerializeField] private Sprite _icon;
+    
+    protected override void Start()
     {
-        Icon = icon;
-        Power = _power;
-        Chargable = true;
+        Name = "Glove";
+        Icon = _icon;
+        Power = 10;
+        Range = 2;
+        Chargable = false;
         ChargeTime = 2;
-        Power = _power;
         Description = $"Knut can really hit as hard as he charges, the soft glove protects his hand -glove has {Power} damage";
         Cooldown = 0;
-        Range = 2;
-        Equipable = true;
+        IsEquipped = false;
     }
 }
--- Slappy-Knut/Assets/Scripts/Weapons/Hand.cs
+++ Slappy-Knut/Assets/Scripts/Weapons/Hand.cs
@@ -1,18 +1,29 @@
-using System;
 using UnityEngine;
 
 public class Hand : Weapon
 {
-    [SerializeField] private float _power = 4f;
-    public GameObject handPrefab;
-    private void Start()
+    public override string Name { get; set; }
+    public override Sprite Icon { get; set; }
+    public override float Power { get; set; }
+    public override float Range { get; set; }
+    public override bool Chargable { get; set; }
+    public override string Description { get; set; }
+    public override float Cooldown { get; set; }
+    public override float ChargeTime { get; set; }
+    public override bool IsEquipped { get; set; }
+
+    [SerializeField] private Sprite _icon;
+    
+    protected override void Start()
     {
-        Power = _power;
-        Chargable = false; // Do we want to charge the sword for heavier slap attack?
-        Power = 10;
+        Name = "Hand";
+        Icon = _icon;
+        Power = 4;
+        Range = 2;
+        Chargable = false;
+        ChargeTime = 0;
         Description = $"Knut only needs his hands of fury. -Hand has {Power} damage";
         Cooldown = 0;
-        Range = 2;
-        Equipable = true;
+        IsEquipped = false;
     }
 }
