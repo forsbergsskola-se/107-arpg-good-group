c3daedc81d00c637672cfa2e88ed9a4ea4b2f94d
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-12-21 14:38:55 +0100

    #501 Creates portal to bosse

---
+++ Slappy-Knut/Assets/Scripts/Portal/PortalToBosse.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalToBosse : MonoBehaviour
+{
+    public int levelRequirement = 3;
+    
+    
+    private Scene _activeScene;
+    private GameObject _player;
+    private PlayerLevelLogic _levelLogic;
+    private GameObject _glove;
+
+    private void Start()
+    {
+        _activeScene = SceneManager.GetActiveScene();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _levelLogic = _player.GetComponent<PlayerLevelLogic>();
+        _glove = FindObjectOfType<Glove>().gameObject;
+
+        DontDestroyOnLoad(_glove);
+    }
+
+    
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Level: " + _levelLogic.level);
+
+        if (IsEnterPortalPressed() && _levelLogic.level >= levelRequirement)
+        {
+            UsePortal();
+        }
+    }
+
+    public void UsePortal()
+    {
+        if (_activeScene.name == "The_Viking_Village")
+        {
+            SceneManager.LoadScene("BossScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("The_Viking_Village");
+        }
+    }
+
+    private bool IsEnterPortalPressed()
+    {
+        return Input.GetKey(KeyCode.E);
+    }
+}
