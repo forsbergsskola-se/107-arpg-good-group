a66b95f69c993e97db2b7473e8fc45b03bbbba3b
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-12-21 11:08:31 +0100

    #501 - Adds portal criteria
    Used Jacobs PlayerLevelLogic script

--- Slappy-Knut/Assets/Scripts/Portal/Lvl1AndLvl2Portal.cs
+++ Slappy-Knut/Assets/Scripts/Portal/Lvl1AndLvl2Portal.cs
@@ -3,15 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class Lvl1AndLvl2Portal : MonoBehaviour
 {
+    public int levelRequirement = 1;
+    
     private float _distance;
     private Scene _activeScene;
     private GameObject _player;
+    private PlayerLevelLogic _levelLogic;
     private GameObject _glove;
 
     private void Start()
     {
         _activeScene = SceneManager.GetActiveScene();
         _player = GameObject.FindGameObjectWithTag("Player");
+        _levelLogic = _player.GetComponent<PlayerLevelLogic>();
         _glove = FindObjectOfType<Glove>().gameObject;
 
         DontDestroyOnLoad(_glove);
@@ -19,9 +23,11 @@ public class Lvl1AndLvl2Portal : MonoBehaviour
 
     private void Update()
     {
+        
         _distance = Vector3.Distance(_player.transform.position, transform.position);
+        Debug.Log("Level: " + _levelLogic.level);
 
-        if (_distance < 3 && IsEnterPortalPressed())
+        if (_distance < 3 && IsEnterPortalPressed() && _levelLogic.level >= levelRequirement)
         {
             UsePortal();
         }
