96ea44a09d98f6b6253d437ae4864aed79913294
nihanbir <nihan.bir@gmail.com>
2022-12-28 20:29:47 +0100

    #022 Fixes NPC step sound issues

--- Slappy-Knut/Assets/Scripts/Enemies/NPC/NPCAudioManager.cs
+++ Slappy-Knut/Assets/Scripts/Enemies/NPC/NPCAudioManager.cs
@@ -3,26 +3,27 @@ using UnityEngine;
 public class NPCAudioManager : MonoBehaviour
 {
     [HideInInspector] public AudioSource AS_FootSteps;
-    
-    [SerializeField] AudioClip footSteps;
     [HideInInspector] public AudioSource AS_Damage;
     [HideInInspector] public AudioSource AS_Hit;
-    [SerializeField] AudioClip hit;
     
+    [SerializeField] AudioClip footSteps;
+    [SerializeField] AudioClip hit;
     [SerializeField] AudioClip damage;
     private void Start()
     {
         AS_FootSteps = gameObject.AddComponent<AudioSource>();
         AS_FootSteps.clip = footSteps;
         AS_FootSteps.spatialBlend = 1;
+        AS_FootSteps.rolloffMode = AudioRolloffMode.Linear;
+        AS_FootSteps.maxDistance = 35;
+        AS_FootSteps.volume = .5f;
+        
 
         AS_Damage = gameObject.AddComponent<AudioSource>();
         AS_Damage.clip = damage;
-        AS_Damage.spatialBlend = 1;
 
         AS_Hit = gameObject.AddComponent<AudioSource>();
         AS_Hit.clip = hit;
-        AS_Hit.spatialBlend = 1;
     }
     
 }
