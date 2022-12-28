8c9cb24d79611d44f8273c52eb88956bf0cd71e8
nihanbir <nihan.bir@gmail.com>
2022-12-28 13:33:11 +0100

    #022 Adds NPC Spawners

--- Slappy-Knut/Assets/Scripts/Spawner.cs
+++ Slappy-Knut/Assets/Scripts/Spawner.cs
@@ -1,19 +1,28 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject knutPrefab;
     public GameObject uiPrefab;
+    public GameObject npcPrefab;
+    public Transform _playerSpawnPoint;
+    public int minNPC = 20;
 
+    private GameObject[] _npcSpawnPoints;
     private GameObject _player;
+    private List<NPC> _npcs = new();
     private static GameObject _ui;
     private Scene _activeScene;
-    private Transform _playerSpawnPoint;
+    private float spawnTime = 5;
     private void Awake()
     {
+        _npcSpawnPoints = GameObject.FindGameObjectsWithTag("NPCSpawnPoint");
         _player = GameObject.FindGameObjectWithTag("Player");
-        _playerSpawnPoint = GameObject.FindGameObjectWithTag("PlayerSpawnPoint").transform;
+        _npcs.AddRange(FindObjectsOfType<NPC>());
         if (_player == null)
         {
             Instantiate(knutPrefab, _playerSpawnPoint.position, Quaternion.Euler(0,0,0));
@@ -27,6 +36,20 @@ public class Spawner : MonoBehaviour
             Instantiate(uiPrefab);
             _ui = GameObject.FindGameObjectWithTag("UI");
             DontDestroyOnLoad(_ui);
+        }
+    }
+
+    private void Update()
+    {
+        spawnTime -= Time.deltaTime;
+        if (spawnTime < 0)
+        {
+            if (_npcs.Count < minNPC)
+            {
+                Vector3 randomSpawnPoint = _npcSpawnPoints[Random.Range(0, _npcSpawnPoints.Length)].transform.position;
+                Instantiate(npcPrefab, randomSpawnPoint, Quaternion.Euler(0, 0, 0));
+            }
+            spawnTime = 5;
         }
     }
 }
