e7ec430d884cb21b5f95dfbf254a2a95802f8893
nihanbir <nihan.bir@gmail.com>
2022-12-29 15:33:22 +0100

    Prepares for merge

--- Slappy-Knut/Assets/Scripts/PauseMenu/PauseGame.cs
+++ Slappy-Knut/Assets/Scripts/PauseMenu/PauseGame.cs
@@ -1,57 +1,29 @@
-using System.Collections;
 using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
     public GameObject canvas;
     public static bool IsPaused;
-    private GameObject _inventory;
-
-    private void Start()
-    {
-        _inventory = GameObject.FindGameObjectWithTag("Inventory");
-    }
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && IsPaused == false)
-        {
-            canvas.SetActive(true);
-            IsPaused = true;
-            _inventory.SetActive(false);
-            Pause();
-        }
-        
-        else if (Input.GetKeyDown(KeyCode.Escape) && IsPaused)
-        {
-            Time.timeScale = 1;
-            IsPaused = false;
-            canvas.SetActive(false);
-            _inventory.SetActive(false);
-            Inventory.DescriptionBox.SetActive(false);
-        }
-
-        if (IsPaused == false)
-        {
-            _inventory.SetActive(true);
-        }
+        if (Input.GetKeyDown(KeyCode.Escape) && !IsPaused) Pause();
+        else if (Input.GetKeyDown(KeyCode.Escape) && IsPaused) ResumeGame();
     }
 
     private void Pause ()
     {
+        canvas.SetActive(true);
+        IsPaused = true;
+        Inventory.inventoryUI.SetActive(false);
         Time.timeScale = 0;
     }
     
     public void ResumeGame()
     {
-        StartCoroutine(WaitForResumeGame());
-        IsPaused = false;
-    }
-    
-    IEnumerator WaitForResumeGame()
-    {
-        yield return new WaitForSecondsRealtime(0.6f);
         Time.timeScale = 1;
+        IsPaused = false;
         canvas.SetActive(false);
+        Inventory.DescriptionBox.SetActive(false);
+        Inventory.inventoryUI.SetActive(false);
     }
 }
