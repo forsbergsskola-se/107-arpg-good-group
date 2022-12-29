70679b8a888e3e17aea9706a6934b98287aa15ef
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-12-29 13:02:24 +0100

    #201 - Updates inventory for pets

--- Slappy-Knut/Assets/Scripts/Player/PlayerInteract.cs
+++ Slappy-Knut/Assets/Scripts/Player/PlayerInteract.cs
@@ -9,6 +9,7 @@ public class PlayerInteract : MonoBehaviour
         if (interactable.GetComponent<Weapon>()) WeaponPickUp(interactable);
         else if (interactable.GetComponent<IConsumable>() != null) ConsumablePickUp(interactable);
         else if (interactable.GetComponent<IDamagable>() != null) GetComponent<PlayerAttack>().AttackAnimation();
+        else if (interactable.GetComponent<Pet>()) PetPickUp(interactable);
     }
     
     void WeaponPickUp(Interactable interactable)
@@ -23,5 +24,12 @@ public class PlayerInteract : MonoBehaviour
         IConsumable c = interactable.gameObject.GetComponent<IConsumable>();
         c.IncreaseCount();
         Destroy(interactable.gameObject);
+    }
+    
+    void PetPickUp(Interactable interactable)
+    {
+        Pet pet = interactable.GetComponent<Pet>();
+        bool wasPickedUp = Inventory.Instance.AddToInventory(pet);
+        if (wasPickedUp) Destroy(interactable.gameObject);
     }
 }
