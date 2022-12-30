9c8ba023472d68e2a1b183ea02680e1e01bc5645
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-12-30 14:03:47 +0100

    #201 - Adds Animations for pet

--- Slappy-Knut/Assets/Scripts/Pets/SjickenMovement.cs
+++ Slappy-Knut/Assets/Scripts/Pets/SjickenMovement.cs
@@ -6,11 +6,12 @@ public class SjickenMovement : MonoBehaviour
 {
     private GameObject _player;
     private Rigidbody _rb;
+    private Animator _anim;
     void Start()
     {
         _player = FindObjectOfType<PlayerAttack>().gameObject;
         _rb = GetComponent<Rigidbody>();
-        //testing
+        _anim = GetComponent<Animator>();
         DontDestroyOnLoad(gameObject);
     }
 
@@ -24,9 +25,12 @@ public class SjickenMovement : MonoBehaviour
         if (Vector3.Distance(_rb.position, _player.transform.position) < 4f)
         {
             //stop the chicken 4f away from player else follow him
+            _anim.SetBool("Run", false);
+            //_anim.SetBool("Eat", true); // dno if we want eat idle
         }
         else
         {
+            _anim.SetBool("Run", true);
             transform.LookAt(_player.transform);
             Vector3 newPos = Vector3.MoveTowards(_rb.position, _player.transform.position, 5f * Time.deltaTime);
             _rb.MovePosition(newPos);    
