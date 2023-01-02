18a7a88f8eb8fe6cf5141dc414586391b9e04372
nihanbir <nihan.bir@gmail.com>
2023-01-02 14:03:45 +0100

    #600 Adds Speed Spell to Player

--- Slappy-Knut/Assets/Scripts/Player/Spells/SpeedSpell.cs
+++ Slappy-Knut/Assets/Scripts/Player/Spells/SpeedSpell.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedSpell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxCoolDown = 10f;
+    public float maxDuration = 3f;
+    public float speedMultiplier = 2f;
+
+    private PlayerMotor _motor;
+    private float _coolDown;
+    private float _duration;
+    private float _movementSpeed;
+    private bool _spellActive;
+
+    private void Start()
     {
-        
+        _motor = FindObjectOfType<PlayerMotor>();
+        _movementSpeed = _motor.agent.speed;
+        _duration = maxDuration;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Alpha2) && _coolDown <= 0)
+        {
+            _spellActive = true;
+            _motor.agent.speed *= speedMultiplier;
+        }
+        
+        if (_spellActive) CastSpell();
+        
+        if (_coolDown < 0) _coolDown = 0;
+        else _coolDown -= Time.deltaTime;
+    }
+
+    private void CastSpell()
+    {
+        _duration -= Time.deltaTime;
+        if (_duration < 0)
+        {
+            _motor.agent.speed = _movementSpeed;
+            _duration = maxDuration;
+            _spellActive = false;
+            _coolDown = maxCoolDown;
+        }
     }
 }
