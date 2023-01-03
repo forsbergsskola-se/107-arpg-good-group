512e46a6727478d66e84c9ee41d90eed19fd7675
nihanbir <nihan.bir@gmail.com>
2023-01-03 17:27:02 +0100

    #600 Removes unused sound from PlayerAudioManager.cs

--- Slappy-Knut/Assets/Scripts/Player/PlayerAudioManager.cs
+++ Slappy-Knut/Assets/Scripts/Player/PlayerAudioManager.cs
@@ -5,22 +5,18 @@ public class PlayerAudioManager : MonoBehaviour
     [HideInInspector] public AudioSource AS_FootSteps;
     [HideInInspector] public AudioSource AS_RageFart;
     [HideInInspector] public AudioSource AS_BasicSlap;
-    [HideInInspector] public AudioSource AS_DrinkPotion;
     
     [SerializeField] AudioClip footSteps;
     [SerializeField] AudioClip rageFart;
     [SerializeField] AudioClip basicSlap;
-    [SerializeField] AudioClip drinkPotion;
     private void Start()
     {
         AS_FootSteps = gameObject.AddComponent<AudioSource>();
         AS_RageFart = gameObject.AddComponent<AudioSource>();
         AS_BasicSlap = gameObject.AddComponent<AudioSource>();
-        AS_DrinkPotion = gameObject.AddComponent<AudioSource>();
         
         AS_FootSteps.clip = footSteps;
         AS_RageFart.clip = rageFart;
         AS_BasicSlap.clip = basicSlap;
-        AS_DrinkPotion.clip = drinkPotion;
     }
 }
