94e4af8aa8dc192ee328d718a5ac82e762eb73cc
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-01-04 17:07:06 +0100

    #802 - Adds Combrat prefab and assets + Combrat script and PetRockScript

---
+++ Slappy-Knut/Assets/Scripts/Enemies/Combrat/PetRock.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetRock : MonoBehaviour
+{
+    private LineRenderer _line;
+    private Combrat _combrat;
+
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _combrat = FindObjectOfType<Combrat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collar();
+    }
+
+    void Collar()
+    {
+    //   _line.SetPosition(0, transform.position);
+//        _line.SetPosition(1, _combrat.transform.position);
+    }
+}
+
