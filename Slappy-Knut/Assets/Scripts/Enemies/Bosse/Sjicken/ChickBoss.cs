931d3e631fef8ed9149661b612a4784eb60991d9
QaleQ <aleflard@gmail.com>
2023-01-16 13:23:52 +0100

    Fixes build version issue and camera zoom bug

--- Slappy-Knut/Assets/Scripts/Enemies/Bosse/Sjicken/ChickBoss.cs
+++ Slappy-Knut/Assets/Scripts/Enemies/Bosse/Sjicken/ChickBoss.cs
@@ -93,7 +93,6 @@ public class ChickBoss : MonoBehaviour
             case StateEnum.Idle:
             default:
                 //unexpected things happened
-                Application.Quit();
                 break;
         }
     }
